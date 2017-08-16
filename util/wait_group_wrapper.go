4d78aaf97585a3802da543142023d0b0420f1781
真人 <weizhen.wang@duitang.com>
2017-08-16 20:27:06 +0800

    feat: support wrapping function and inject

--- util/wait_group_wrapper.go
+++ util/wait_group_wrapper.go
@@ -9,10 +9,18 @@ type WaitGroupWrapper struct {
 	sync.WaitGroup
 }
 
-func (w *WaitGroupWrapper) Wrap(f reflect.Value, p []reflect.Value) {
+func (w *WaitGroupWrapper) ReflectWrap(f reflect.Value, p []reflect.Value) {
 	w.Add(1)
 	go func() {
 		f.Call(p)
+		w.Done()
+	}()
+}
+
+func (w *WaitGroupWrapper) Wrap(cb func()) {
+	w.Add(1)
+	go func() {
+		cb()
 		w.Done()
 	}()
 }
