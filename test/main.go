7079b22f9167717d59178d6475ff9561e6a9bb86
真人 <weizhen.wang@duitang.com>
2017-08-18 19:38:47 +0800

    test: test start and the end of workflow

--- test/main.go
+++ test/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "github.com/hawkingrei/emitter"
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/hawkingrei/emitter/component"
+	"github.com/hawkingrei/emitter/util/inject"
+)
+
+type SpecialString interface{}
+type Year interface{}
+
+type Other struct {
+	Id int
+}
+
+func (o *Other) One(output component.Output) {
+	out := reflect.ValueOf(output)
+	for a := range []int{2, 3, 4} {
+		fmt.Println("one ", a)
+		v := reflect.ValueOf(a)
+		out.Send(v)
+	}
+}
+
+func (o *Other) Two(a int) {
+	fmt.Println(a + o.Id)
+}
 
 func main() {
-	emitter.Run()
+
+	wf := component.NewWorkflow()
+	one := Other{Id: 1}
+	inj := inject.New()
+	wf.Add(one.One, inj, reflect.TypeOf(1))
+	wf.Add(one.Two, inj, reflect.TypeOf(1))
+	wf.Run()
 }
